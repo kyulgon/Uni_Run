d206cd0e64b47e70227d72186861712fd81f01b6
kyulgon <kyulgon@naver.com>
2020-04-30 17:18:29 +0900

    순서 정렬, 무한 배경, UI 구성

---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    public float speed = 10f;   // 이동속도
+
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
